20c4a3e5d0d1b358c6930c37d4296bfacbaa8ed7
ThibautG <thibaut.gaudinat@gmail.com>
2025-01-27 13:37:53 +0100

    gestion des couleurs des boutons quand on clique

---
+++ index.js
@@ -0,0 +1,86 @@
+// récupération de chaque bouton dans une variable correspondante
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const btn4 = document.getElementById("btn-4");
+const btn5 = document.getElementById("btn-5");
+const btn6 = document.getElementById("btn-6");
+const btn7 = document.getElementById("btn-7");
+const btn8 = document.getElementById("btn-8");
+const btn9 = document.getElementById("btn-9");
+const btn10 = document.getElementById("btn-10");
+const btn11 = document.getElementById("btn-11");
+const btn12 = document.getElementById("btn-12");
+const btn13 = document.getElementById("btn-13");
+const btn14 = document.getElementById("btn-14");
+const btn15 = document.getElementById("btn-15");
+const btn16 = document.getElementById("btn-16");
+const btn17 = document.getElementById("btn-17");
+const btn18 = document.getElementById("btn-18");
+const btn19 = document.getElementById("btn-19");
+const btn20 = document.getElementById("btn-20");
+
+
+
+// appliquer la couleur de fond correspondant à la réponse quand on clique dessus sur le bouton
+btn1.addEventListener("click", () => {
+    btn1.style.background = "green";
+})
+btn2.addEventListener("click", () => {
+    btn2.style.background = "red";
+})
+btn3.addEventListener("click", () => {
+    btn3.style.background = "red";
+})
+btn4.addEventListener("click", () => {
+    btn4.style.background = "green";
+})
+btn5.addEventListener("click", () => {
+    btn5.style.background = "green";
+})
+btn6.addEventListener("click", () => {
+    btn6.style.background = "red";
+})
+btn7.addEventListener("click", () => {
+    btn7.style.background = "green";
+})
+btn8.addEventListener("click", () => {
+    btn8.style.background = "red";
+})
+btn9.addEventListener("click", () => {
+    btn9.style.background = "red";
+})
+btn10.addEventListener("click", () => {
+    btn10.style.background = "green";
+})
+btn11.addEventListener("click", () => {
+    btn11.style.background = "green";
+})
+btn12.addEventListener("click", () => {
+    btn12.style.background = "red";
+})
+btn13.addEventListener("click", () => {
+    btn13.style.background = "green";
+})
+btn14.addEventListener("click", () => {
+    btn14.style.background = "red";
+})
+btn15.addEventListener("click", () => {
+    btn15.style.background = "red";
+})
+btn16.addEventListener("click", () => {
+    btn16.style.background = "green";
+})
+btn17.addEventListener("click", () => {
+    btn17.style.background = "red";
+})
+btn18.addEventListener("click", () => {
+    btn18.style.background = "green";
+})
+btn19.addEventListener("click", () => {
+    btn19.style.background = "green";
+})
+btn20.addEventListener("click", () => {
+    btn20.style.background = "red";
+})
+
